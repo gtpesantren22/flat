e39f2acdfe5155247d782a6fdd23e29562da1346
gtpesantren22 <cakromat26@gmail.com>
2025-11-27 19:32:58 +0700

    update: fix proses pengambilan data karyawan

--- application/controllers/Kinerja.php
+++ application/controllers/Kinerja.php
@@ -91,7 +91,7 @@ class Kinerja extends MY_Controller
 
         $bulan = $this->input->post('bulan', true);
         $tahun = $this->input->post('tahun', true);
-        $guru = $this->db_active->query("SELECT guru_id FROM guru JOIN kategori ON guru.kategori=kategori.id WHERE kategori.nama = 'Karyawan' ")->result();
+        $guru = $this->db_active->query("SELECT guru_id FROM guru WHERE kriteria = 'Karyawan' ")->result();
         $at = date('Y-m-d H:i');
         $id = $this->uuid->v4('');
         foreach ($guru as $value) {
@@ -129,13 +129,13 @@ class Kinerja extends MY_Controller
             $this->db_active->from('kehadiran');
             $this->db_active->join('guru', 'kehadiran.guru_id=guru.guru_id');
             $this->db_active->where('kehadiran_id', $kehadiranID);
-            $this->db_active->where('guru.kategori', 5);
+            $this->db_active->where('guru.kriteria', 'Karyawan');
         } else {
             $kehadiranID = $this->db_active->query("SELECT kehadiran_id FROM kehadiran GROUP BY kehadiran_id ORDER BY created_at DESC LIMIT 1")->row('kehadiran_id');
             $this->db_active->from('kehadiran');
             $this->db_active->join('guru', 'kehadiran.guru_id=guru.guru_id');
             $this->db_active->where('kehadiran.kehadiran_id', $kehadiranID);
-            $this->db_active->where('guru.kategori', 5);
+            $this->db_active->where('guru.kriteria', 'Karyawan');
         }
 
 
@@ -211,7 +211,7 @@ class Kinerja extends MY_Controller
         $id = $this->input->post('id', true);
         $kehadiran = $this->model->getBy('kehadiran', 'id', $id)->row();
 
-        $guru = $this->db_active->query("SELECT * FROM guru WHERE NOT EXISTS (SELECT 1 FROM kehadiran WHERE kehadiran_id = '$kehadiran->kehadiran_id' AND kehadiran.guru_id = guru.guru_id) AND kategori = 5 ");
+        $guru = $this->db_active->query("SELECT * FROM guru WHERE NOT EXISTS (SELECT 1 FROM kehadiran WHERE kehadiran_id = '$kehadiran->kehadiran_id' AND kehadiran.guru_id = guru.guru_id) AND kriteria = 'Karyawan' ");
         if ($guru->row()) {
             foreach ($guru->result() as $value) {
                 $data = [
